19dcb54787e843ced30fbed10ddc97e92715915f
r-caamano <robert.caamano@netfoundry.io>
2023-03-23 23:13:03 +0000

    refactored map_update tuning performance

--- src/map_update.c
+++ src/map_update.c
@@ -531,7 +531,7 @@ bool set_diag(int *idx){
     return true;
 }
 
-bool interface_map()
+bool interface_diag()
 {
     /* create bpf_attr to store ifindex_ip_map */
     union bpf_attr if_map;
@@ -622,6 +622,95 @@ bool interface_map()
                 if(!strcmp(prefix_interface, address->ifa_name)){
                     set_diag(&idx);
                 }
+            }
+            int ret = syscall(__NR_bpf, BPF_MAP_UPDATE_ELEM, &if_map, sizeof(if_map));
+            if (ret)
+            {
+                printf("MAP_UPDATE_ELEM: %s \n", strerror(errno));
+                close(if_fd);
+                exit(1);
+            }
+        }
+        net_count++;
+        address = address->ifa_next;
+    }
+    close(if_fd);
+    freeifaddrs(addrs);
+    return create_route;
+}
+
+bool interface_map()
+{
+    /* create bpf_attr to store ifindex_ip_map */
+    union bpf_attr if_map;
+    /*path to pinned ifindex_ip_map*/
+    const char *if_map_path = "/sys/fs/bpf/tc/globals/ifindex_ip_map";
+    struct ifaddrs *addrs;
+
+    /* call function to get a linked list of interface structs from system */
+    if (getifaddrs(&addrs) == -1)
+    {
+        printf("can't get addrs");
+        exit(1);
+    }
+    struct ifaddrs *address = addrs;
+    /* open BPF ifindex_ip_map */
+    memset(&if_map, 0, sizeof(if_map));
+    /* set path name with location of map in filesystem */
+    if_map.pathname = (uint64_t)if_map_path;
+    if_map.bpf_fd = 0;
+    if_map.file_flags = 0;
+    /* make system call to get fd for map */
+    int if_fd = syscall(__NR_bpf, BPF_OBJ_GET, &if_map, sizeof(if_map));
+    if (if_fd == -1)
+    {
+        printf("BPF_OBJ_GET: %s \n", strerror(errno));
+        exit(1);
+    }
+    if_map.map_fd = if_fd;
+    int idx = 0;
+    /*
+     * traverse linked list of interfaces and for each non-loopback interface
+     *  populate the index into the map with ifindex as the key and ip address
+     *  as the value
+     */
+    int net_count = 0;
+    struct sockaddr_in *ipaddr;
+    in_addr_t ifip;
+    int ipcheck = 0;
+    bool create_route = true;
+    while (address)
+    {
+        if (address->ifa_addr && (address->ifa_addr->sa_family == AF_INET))
+        {
+            get_index(address->ifa_name, &idx);
+            if (strncmp(address->ifa_name, "lo", 2))
+            {
+                ipaddr = (struct sockaddr_in *)address->ifa_addr;
+                ifip = ipaddr->sin_addr.s_addr;
+                struct sockaddr_in *network_mask = (struct sockaddr_in *)address->ifa_netmask;
+                __u32 netmask = ntohl(network_mask->sin_addr.s_addr);
+                ipcheck = is_subset(ntohl(ifip), netmask, ntohl(dcidr.s_addr));
+                if(!ipcheck)
+                {
+                    create_route = false;
+                }
+            }
+            else
+            {
+               ifip = 0x0100007f;
+            }
+            struct ifindex_ip4 ifip4 = {
+                ifip,
+                {0}};
+            sprintf(ifip4.ifname, "%s", address->ifa_name);
+            if_map.key = (uint64_t)&idx;
+            if_map.flags = BPF_ANY;
+            if_map.value = (uint64_t)&ifip4;
+            if(all_interface){
+                echo_interface = address->ifa_name;
+                verbose_interface = address->ifa_name;
+                prefix_interface = address->ifa_name;
             }
             int ret = syscall(__NR_bpf, BPF_MAP_UPDATE_ELEM, &if_map, sizeof(if_map));
             if (ret)
@@ -1258,17 +1347,17 @@ int main(int argc, char **argv)
     }
 
     if(verbose){
-        interface_map();
+        interface_diag();
         exit(0);
     }
 
     if(echo){
-        interface_map();
+        interface_diag();
         exit(0);
     }
 
     if(per_interface){
-        interface_map();
+        interface_diag();
         exit(0);
     }
 
